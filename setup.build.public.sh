6638d6992a1093b1995a3151c80d1c08be6e3971
Workshop Participant <participant@example.com>
2025-03-29 06:11:24 +0000

    app runner

--- setup.build.public.sh
+++ setup.build.public.sh
@@ -14,10 +14,5 @@ export GITHUB_REPO="https://github.com/snunezcode/mytagger.git"
 aws cloudformation create-stack \
   --stack-name "build-$IDENTIFIER" \
   --template-body file://cloudformation.public.yaml \
-  --parameters \
-    ParameterKey=GitHubRepositoryUrl,ParameterValue=$GITHUB_REPO \
-    ParameterKey=AppUser,ParameterValue=$APP_USER \
-    ParameterKey=IPv4CIDR,ParameterValue=$IPV4_CIDR \
-    ParameterKey=IPv6CIDR,ParameterValue=$IPV6_CIDR \    
-  --capabilities CAPABILITY_IAM
+  --parameters ParameterKey=GitHubRepositoryUrl,ParameterValue=$GITHUB_REPO ParameterKey=AppUser,ParameterValue=$APP_USER ParameterKey=IPv4CIDR,ParameterValue=$IPV4_CIDR ParameterKey=IPv6CIDR,ParameterValue=$IPV6_CIDR --capabilities CAPABILITY_IAM
  
