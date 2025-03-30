b00e113a8e627316874c16bed8a83aa5aa972a09
Workshop Participant <participant@example.com>
2025-03-30 19:12:52 +0000

    app runner

--- setup.frontend.sh
+++ setup.frontend.sh
@@ -53,7 +53,7 @@ RUN dnf update -y && \
     dnf install -y nginx procps shadow-utils && \
     dnf clean all
 
-COPY nginx.conf /etc/nginx/nginx.conf
+COPY ./nginx.conf /etc/nginx/nginx.conf
 
 RUN rm -rf /usr/share/nginx/html/*
 COPY ./build/ /usr/share/nginx/html/
